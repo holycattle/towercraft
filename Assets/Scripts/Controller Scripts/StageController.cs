640ab5486902f19360a4472a2392df6365208791
Joniel Ibasco <jlibasco@gmail.com>
2012-12-02 21:53:59 +0800

    began work on event handling

--- Assets/Scripts/Controller Scripts/StageController.cs
+++ Assets/Scripts/Controller Scripts/StageController.cs
@@ -5,11 +5,14 @@ public class StageController : MonoBehaviour {
 	private Hashtable bossStages = new Hashtable();
 	private WaveController waveController;
 	//public GameMessage gameMessages[];
+	
 	public int finalWave = -1;
+	
+	public Hashtable specialEvents = new Hashtable();
 	
 	public StageController(int f) {
 		finalWave = f;
-		waveController = GameObject.Find(" GameController").GetComponentInChildren<WaveController>();
+		//waveController = GameObject.Find(" GameController").GetComponentInChildren<WaveController>();
 	}
 	
 
@@ -17,12 +20,16 @@ public class StageController : MonoBehaviour {
 	void Start () {
 		
 		waveController = GameObject.Find(" GameController").GetComponentInChildren<WaveController>();
+		
+		//hard code special events for now
+		//specialEvents[] = 
 	}
 	
 	// Update is called once per frame
 	public virtual void Update () {
 		if(waveController.waveNumber != -1 && waveController.waveNumber > finalWave) {
 			//do victory scene/action
+			//Camera.mainCamera.GetComponent<Skybox>().material = Resources.Load("Skyboxes/Skybox17", typeof(Material)) as Material;
 			Debug.Log("Victory!");
 		}
 	}
