39416f4a216f4c6df0d3ac124df1af073d169aea
Joniel Ibasco <jlibasco@gmail.com>
2012-12-03 19:17:51 +0800

    setup StageController to listen to WaveEvents and TimeEvents

--- Assets/Scripts/Controller Scripts/StageController.cs
+++ Assets/Scripts/Controller Scripts/StageController.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 public class StageController : MonoBehaviour {
 	private Hashtable bossStages = new Hashtable();
 	private WaveController waveController;
-	public List<Event> events;
+	public List<TimeEvent> timeEvents;
+	public List<WaveEvent> waveEvents;
 	
 	public int finalWave = -1;
 	
@@ -20,45 +21,32 @@ public class StageController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		waveController = GameObject.Find(" GameController").GetComponentInChildren<WaveController>();
-		events = new List<Event>();
-		Event e = new Event();
-		Hashtable args = new Hashtable();
-		//Debug.Log("current wave: " + waveController.waveNumber.ToString());
-		args[0] = waveController;
-		args[1] = finalWave;
-		e.addAction(newSkybox, args);
-		e.addCondition(isVictorious, args);
-		events.Add(e);
-		Debug.Log ("Added " + events.ToString());
+		timeEvents = new List<TimeEvent>();
+		waveEvents = new List<WaveEvent>();
+		
+		//initialize events here
 	}
 	
 	// Update is called once per frame
 	public virtual void Update () {
-		//if(waveController.waveNumber != -1 && waveController.waveNumber > finalWave) {
-			//do victory scene/action
-			//Camera.mainCamera.GetComponent<Skybox>().material = Resources.Load("Skyboxes/Skybox17", typeof(Material)) as Material;
-		//}
-		//Event e = new Event(, null);
 		listenForEvents();
 	}
 	
 	public void newSkybox(Hashtable h) {
 		Debug.Log ("should change skybox here");
-		Camera.mainCamera.GetComponent<Skybox>().material = Resources.Load("Skyboxes/Skybox17", typeof(Material)) as Material;
-	}
-	
-	public bool isVictorious(Hashtable h) {
-		Debug.Log("final wave = " + ((int)h[1]).ToString());
-		int w = ((WaveController)h[0]).waveNumber;
-		Debug.Log("current wavenumber = " + w.ToString());
-		return w == (int)h[1];
+		RenderSettings.skybox = Resources.Load("Skyboxes/Skybox17", typeof(Material)) as Material;
 	}
 	
 	public void listenForEvents() {
 		//this needs garbage collection
-		foreach(Event eventItr in events) {
-			if(!eventItr.isSatisfied) { //minor optimization... I really need to find a way to really optimize this
-				eventItr.evalCondition();
+		foreach(TimeEvent t in timeEvents) {
+			if(!t.isSatisfied) { //minor optimization... I really need to find a way to really optimize this
+				t.evalCondition();
+			}
+		}
+		foreach(WaveEvent w in waveEvents) {
+			if(!w.isSatisfied) { //minor optimization... I really need to find a way to really optimize this
+				w.evalCondition();
 			}
 		}
 	}
