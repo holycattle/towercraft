4c21a4a3957d8f0e6fed982442edba3e4e578903
Joniel Ibasco <jlibasco@gmail.com>
2012-12-03 19:18:29 +0800

    changed implementation of skybox; skybox is now rendered through RenderSettings instead of the main camera

--- Assets/Scripts/Wave Scripts/Tank.cs
+++ Assets/Scripts/Wave Scripts/Tank.cs
@@ -13,7 +13,7 @@ public class Tank : SpawnScheme {
 		int health = (int)(((1f / moveSpeed) * HEALTH_COEFF) * (1 + (waveNumber * HEALTH_MULTIPLIER)));
 		float interval = ((1f / moveSpeed) * GetINTERVAL_COEFF);
 
-		while (cost > 0) {
+		while(cost > 0) {
 			// Choose which mob to spawn.
 			GameObject g = (GameObject)mobTable[enemyType.ToString()]; //optimized assigning of new mob by using a Hashtable
 			_spawnScheme.Add(new MobSpawn(g, interval, moveSpeed, health, waveNumber, resistanceType, UnityEngine.Random.Range(0.5f, 1f)));
