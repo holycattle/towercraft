7f2320e07956f9ffcf17e369c4fe09fe9aaf53c3
Joniel Ibasco <jlibasco@gmail.com>
2012-12-05 02:16:18 +0800

    minimap camera movement based on mouse pointer position

--- Assets/Scripts/Controller Scripts/CameraController.cs
+++ Assets/Scripts/Controller Scripts/CameraController.cs
@@ -18,8 +18,20 @@ public class CameraController : MonoBehaviour {
 	private const float INTERVAL = 0.25f;
 	private GameObject pathDrawer;
 	private float pathInterval = 0;
+	
+	private Vector3 mousePos;
+	//private float terrainWidth;
+	//private float terrainHeight;
+	const float SCROLL_WIDTH = 10f;
+	const float SCROLL_HEIGHT = 10f;
+	const int SCROLL_SPEED = 32;
 
 	void Start() {
+		//Terrain t = GameObject.Find("[Root]World").GetComponent<Terrain>();
+		//terrainWidth = t.terrainData.size.y;
+		//terrainHeight = t.terrainData.size.x;
+		//Debug.Log("terrain width = " + terrainWidth.ToString());
+		//Debug.Log("terrain height = " + terrainHeight.ToString());
 		minimapCam = GameObject.Find("Minimap Camera").GetComponent<Camera>();
 		firstPersonCam = GameObject.Find("Main Camera").GetComponent<Camera>();
 		//set default skybox
@@ -44,12 +56,34 @@ public class CameraController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.C)) {
 			SetOverviewCamera(!minimapCam.enabled);
 		}
-
+		
+		int top = 0; //1 if going up, -1 if going down
+		int left = 1; //1 if going right, -1 if going left
+		float forwardMoveAmount;
+		float sideMoveAmount;
 		if (minimapCam.enabled) {
-			float forwardMoveAmount = Input.GetAxis("Vertical") * forwardSpeed * Time.deltaTime;
-			float sideMoveAmount = Input.GetAxis("Horizontal") * forwardSpeed * Time.deltaTime;
+			mousePos = Input.mousePosition;
+			//Debug.Log("x: " + mousePos.x.ToString() + "; y: " + mousePos.y.ToString());
+			//Debug.Log("screen width: " + Screen.width.ToString() + "; screen height: " + Screen.height.ToString());
+			if(mousePos.x < SCROLL_WIDTH) {
+				left = -1;
+			} else if(mousePos.x >= Screen.width - SCROLL_WIDTH)
+				left = 1;
+			else left = 0;
+			
+			if(mousePos.y <= 0) {
+				top = -1;
+			} else if(mousePos.y >= Screen.height)
+				top = 1;
+			else top = 0;
+			
+			forwardMoveAmount = top * SCROLL_SPEED * Time.deltaTime;
+			sideMoveAmount = left * SCROLL_SPEED * Time.deltaTime;
+			//legacy support for WSAD movement
+			forwardMoveAmount = Input.GetAxis("Vertical") * SCROLL_SPEED * Time.deltaTime;
+			sideMoveAmount = Input.GetAxis("Horizontal") * SCROLL_SPEED * Time.deltaTime;
 			transform.Translate(sideMoveAmount, 0, forwardMoveAmount, Space.World);
-
+			
 			// Path Drawers
 			if (Input.GetKeyDown(KeyCode.Q)) {
 				GameObject g = Instantiate(pathDrawer) as GameObject;
