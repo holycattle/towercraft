1c821c78b759ebe4457c7c62441ef2848f82d666
Joniel Ibasco <jlibasco@gmail.com>
2012-12-05 03:44:27 +0800

    removed exit button for webplayer build

--- Assets/Scripts/Menu Scripts/MainMenu/StartScreen.cs
+++ Assets/Scripts/Menu Scripts/MainMenu/StartScreen.cs
@@ -12,13 +12,8 @@ public class StartScreen : MonoBehaviour {
 		Rect startGameRect = new Rect((Screen.width - BUTTON_WIDTH)/2, (Screen.height - BUTTON_HEIGHT)/2 + 50, BUTTON_WIDTH, BUTTON_HEIGHT);
 		bool startGame = GUI.Button(startGameRect, "Start Game");
 		
-		Rect exitGameRect = new Rect((Screen.width - BUTTON_WIDTH)/2, (Screen.height - BUTTON_HEIGHT)/2 + 150, BUTTON_WIDTH, BUTTON_HEIGHT);
-		bool exitGame = GUI.Button(exitGameRect, "Exit Game");
 //		Debug.Log("test");
 		if(startGame)
 			AutoFade.LoadLevel("Main" ,3,1,Color.black);
-		
-		if(exitGame)
-			Application.Quit();
 	}
 }
